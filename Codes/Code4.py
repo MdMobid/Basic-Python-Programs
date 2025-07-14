75ebb31820fb97df6b9f6937c6946f187510d633
MdMobid <mobid9021@outlook.com>
2025-07-14 10:50:14 +0530

    🔄️ Code 4
    Write A Program To Take Input for a Number and check whether its a Part of Fibonacci Series (upto 80)

--- Codes/Code4.py
+++ Codes/Code4.py
@@ -1,16 +1,19 @@
-# Write A Program To Take Input for a Number and check whether its a Part of Fibonacci Series (upto 80):
+# Write A Program To Take Input for a Number and check whether its a Part of Fibonacci Series (upto 80)
 
 num=int(input("Enter a Number: "))
+nterms=20
 list1=[]
-m=0
-n=1
+n1=0
+n2=1
+count=0
 c=0
 
-while n<89:
-  a=m+n
-  list1.append(m)
-  m=n
-  n=a
+while count<nterms:
+  a=n1+n2
+  list1.append(n1)
+  n1=n2
+  n2=a
+  count+=1
 
 for i in range (0,len(list1)):
   if list1[i]==num:
