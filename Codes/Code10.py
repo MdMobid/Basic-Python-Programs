18367631818257a7274615f5ebd992ecd3dbf513
MdMobid <mobid9021@outlook.com>
2025-07-14 12:03:41 +0530

    + Code 10
    Write A Program To Interchange The First Element with Last and so on:

---
+++ Codes/Code10.py
@@ -0,0 +1,20 @@
+# Write A Program To Interchange The First Element with Last and so on:
+
+print("Enter All Numbers You Want in the List with Comma Seperation: ")
+print("For Example: 1,2,3,4,5 \n")
+
+list2=[]
+list1=eval(input(">> "))
+list2.extend(list1)
+list3=list2[::]
+print("")
+
+e=len(list2)-1
+for i in range (0,int(len(list2)/2)):
+  t=list2[i]
+  list2[i]=list2[e]
+  list2[e]=t
+  e=e-1
+
+print("Input List:",list3)
+print("Interchanged List:",list2)
