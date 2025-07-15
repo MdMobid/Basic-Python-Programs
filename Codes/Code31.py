aa5ce3c28c67e411f846e2533ce6ed1bdfe016c5
MdMobid <mobid9021@outlook.com>
2025-07-15 10:17:29 +0530

    + Code 31 Write a Python program to delete the first 5 elements from the list using slicing and print the updated list

---
+++ Codes/Code31.py
@@ -0,0 +1,6 @@
+# Write a Python program to delete the first 5 elements from the list using slicing and print the updated list.
+
+list1=[25,30,56,12,45,21,7,20] # Given list
+
+del list1[:5]
+print(list1)
